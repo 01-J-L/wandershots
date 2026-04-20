80087ba6f26e37e6e4f9fe7f425526ef5b554edf
01-JL <enemyslayer0909@gmail.com>
2026-04-20 19:10:08 +0800

    update

--- website/static/sw.js
+++
@@ -1,8 +0,0 @@
-// A simple service worker to satisfy PWA installation requirements
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Leave empty: bypasses caching, ensuring your dynamic data is always fresh
-});
